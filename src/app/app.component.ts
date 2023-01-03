43636d54d5ba7b8adbd6ea12bd307c1fa4f1eb88
unknown <daniellamoran22@gmail.com>
2023-01-03 17:21:02 +0200

    done

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+//Component declaration
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+//Assigning types to (variables?) is recommended
+export class AppComponent {
+
+}
